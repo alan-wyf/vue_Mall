43bb309f4e449e57898dac3cf67eea5585898eb2
alan-wyf <wwwyf0817@gmail.com>
2020-12-22 15:07:59 +0800

    更改

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+import api from '@/assets/config/api.js'
+import axios from 'axios'
 
+Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
+    cartTotal: {
+      goodsCount: 0,
+      goodsAmount: 0,
+      checkedGoodsCount: 0,
+      checkedGoodsAmount: 0
+    },
+    cartList: []
+
   },
   mutations: {
+    setCarList: function (state, cartList) {
+      state.cartList = cartList
+    },
+    setCartTotal: function (state, cartTotal) {
+      state.cartTotal = cartTotal
+    }
   },
   actions: {
+    AjaxCart: async function (content) {
+      const cartRes = await axios.get(api.CartList)
+      console.log(cartRes.data)
+      content.commit('setCarList', cartRes.data.data.cartList)
+      content.commit('setCartTotal', cartRes.data.data.cartTotal)
+    }
   },
   modules: {
   }
